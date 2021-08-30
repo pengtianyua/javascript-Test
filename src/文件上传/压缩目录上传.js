8f47977b65bd15b116faea462cac3f4cc241589d
Pengtianyu <2501846063@qq.com>
2021-08-30 15:15:17 +0800

    压缩目录上传

---
+++ src/文件上传/压缩目录上传.js
@@ -0,0 +1,47 @@
+//  https://www.npmjs.com/package/jszip
+
+const uploadFileEle = document.querySelector("#uploadFile");
+
+/**
+ * @description 将目录下的文件列表压缩成一个ZIP
+ * */
+function generateZipFile(
+	zipName, files,
+	options = { type: "blob", compression: "DEFLATE" }
+) {
+	return new Promise((resolve, reject) => {
+		const zip = new JSZip();
+
+		for (let i = 0; i < files.length; i++) {
+			zip.file(files[i].webkitRelativePath, files[i]);
+		}
+
+		zip.generateAsync(options).then(blob => {
+			zipName = zipName || Date.now() + ".zip";
+			const zipFile = new File([blob], zipName, {
+				type: "application/zip"
+			});
+			resolve(zipFile);
+		});
+	});
+}
+
+async function uploadFile() {
+	let fileList = uploadFileEle.files;
+	if (!fileList.length) return;
+	let webkieRelativePath = fileList[0].webkitRelativePath;
+	let zipFileName = webkieRelativePath.split("/")[0] + "zip";
+	let zipFile = await generateZipFile(zipFileName, fileList);
+	upload({
+		url: "",
+		file: zipFile,
+		fileName: zipFileName
+	});
+}
+
+function upload({ url, file, fileName, fieldName = "file" }) {
+	if(!url || !file) return
+	let formData = new FormData()
+	formData.append(fieldName, file, fileName)
+	request.post(url, formData)
+}

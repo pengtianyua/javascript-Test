b27c4b02cba4e927702f72d1a2d443493f4e43ed
Pengtianyu <2501846063@qq.com>
2022-01-28 16:18:48 +0800

    feat: 函数防抖

--- src/手写/debounce.js
+++ src/手写/debounce.js
@@ -1,15 +1,21 @@
 /**
- * @method debounce
- * @description 函数防抖
- * @param fn 需要防抖的函数
- * @param delay 延迟时间
- * */
-export function debounce(fn, delay) {
+ *
+ * @param fn
+ * @param delay
+ * @param flag
+ * @returns {(function(...[*]=): void)|*}
+ *
+ * 有时需要让函数立即执行一次 再等后面事件触发后等待n秒执行 给 debounce 函数一个 flag 用于标识是否立即执行
+ */
+export function debounce(fn, delay, flag) {
 	let timeout = null;
-	return function () {
+	return function (...args) {
 		clearTimeout(timeout);
+		if(flag && !timeout) { // 当定时器变量为空时 说明是第一次执行 就立即执行一次
+			fn.apply(this, args)
+		}
 		timeout = setTimeout(() => {
-			fn.apply(this, arguments);
+			fn.apply(this, args);
 		}, delay);
 	};
 }

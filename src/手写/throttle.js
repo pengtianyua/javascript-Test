5c7a9929751973809dafbdeee0a065df09a8baaa
Pengtianyu <2501846063@qq.com>
2022-01-28 16:14:24 +0800

    feat: 函数节流

--- src/手写/throttle.js
+++ src/手写/throttle.js
@@ -1,17 +1,37 @@
 /**
- * @method throttle
- * @description 函数节流
- * @param fn 需要节流的函数
- * @param delay 延迟时间
- * */
+ *
+ * @param fn
+ * @param delay
+ * @returns {(function(...[*]=): void)|*}
+ *
+ * 第一次事件不会触发 最后一次一定触发
+ */
 export function throttle(fn, delay) {
-	let canRun = true;
-	return function () {
-		if (!canRun) return;
-		canRun = false;
-		setTimeout(() => {
-			fn.apply(this, arguments);
-			canRun = true;
-		}, delay);
+	let timer = null
+	return function (...args) {
+		if (!timer) {
+			timer = setTimeout(() => {
+				timer = null
+				fn.apply(this, args);
+			}, delay);
+		}
 	};
+}
+
+/**
+ *
+ * @param fn
+ * @param time
+ * @returns {(function(...[*]=): void)|*}
+ *
+ * 第一次事件肯定触发 最后一次不会触发
+ */
+export function throttle(fn, time) {
+	let pre = 0;
+	return function(...args) {
+		if(Date.now() - pre > time) {
+			pre = Date.now()
+			fn.apply(this, args)
+		}
+	}
 }

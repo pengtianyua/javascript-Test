e8f617ef2864a0a8e52d24f70d74a55604b82e1c
Pengtianyu <2501846063@qq.com>
2022-01-28 16:15:09 +0800

    feat: 选择排序

--- src/JS/数组排序/selectSort.js
+++ src/JS/数组排序/selectSort.js
@@ -1,20 +1,22 @@
 /**
- * @method 选择排序
- * @description 把当前元素分别跟后面所有的元素对比 把最小的逐个往前排列
- * @param arr
- * @return arr
- * */
-function selectSort(arr) {
-	for (let i = 0; i < arr.length; i++) {
-		for (let j = i + 1; j < arr.length; j++) {
-			if (arr[i] > arr[j]) {
-				let current = arr[i];
-				arr[i] = arr[j];
-				arr[j] = current;
+ * 每次循环选取一个最小的数字放到前面的有序序列中
+ *
+ * 时间复杂度 O(n2)
+ * 空间复杂度 O(1)
+ *
+ * @param array
+ */
+function selectSort(array) {
+	for (let i = 0; i < array.length - 1; i++) {
+		let minIndex = i;
+		for (let j = i + 1; j < array.length; j++) {
+			if (array[j] < array[minIndex]) {
+				minIndex = j;
 			}
 		}
+		[array[minIndex], array[i]] = [array[i], array[minIndex]];
 	}
-	console.log(arr);
 }
-const arr = [12,3,44,343,55,1,23]
+
+const arr = [10, 8, 20, 5, 6, 30, 11, 9];
 selectSort(arr)

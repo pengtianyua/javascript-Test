506a3b7e6f11ceda9ee3c77851cad18da4d73fb9
Pengtianyu <2501846063@qq.com>
2022-01-28 16:19:30 +0800

    feat: 函数柯里化

--- src/FnCurrying/FnCurrying.js
+++ src/FnCurrying/FnCurrying.js
@@ -1,12 +1,16 @@
-function sum(a) {
-	return function (b) {
-		return a + b;
-	};
+/**
+ *
+ * @param fn
+ * @param args
+ * @returns {any}
+ *
+ * 判断当前函数传入的参数是否大于或等于 fn 需要参数的数量，如果是 直接执行 fn
+ * 如果传入参数数量不够 返回一个闭包 暂存传入的参数 并重新返回 currying 函数
+ */
+function currying(fn, ...args) {
+	if (args.length >= fn.length) {
+		return fn(...args);
+	} else {
+		return (...args2) => currying(fn, ...args, ...args2);
+	}
 }
-
-const a = sum(1)(2);
-console.log(a);
-
-const b = sum(1);
-const c = b(2);
-console.log(c);

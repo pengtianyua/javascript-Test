147ad7a926acd89f65af1987a077b3a8e1116bef
Pengtianyu <2501846063@qq.com>
2022-01-28 16:17:05 +0800

    feat: 继承

--- src/Inherit/Inherit.js
+++ src/Inherit/Inherit.js
@@ -1,23 +1,69 @@
+function People() {
+	this.type = "people";
+}
+
+People.prototype.eat = function() {
+	console.log("吃东西");
+};
+
+function Man(name) {
+	this.name = name;
+	this.color = "black";
+}
+
 /**
- * 原型链继承
- * @description 使用父类的实例重写子类的原型
+ * 原型继承
+ *
+ * 原型是所有子类实例共享的 改变一个其他也会改变
  */
-function SuperType() {
-	this.property = true;
+Man.prototype = new People();
+
+/**
+ * 构造继承 在子类构造函数中调用父类构造函数
+ *
+ * 不能继承父类原型 函数在构造函数中 每个子类实例不能共享函数 浪费内存
+ */
+function Man(name) {
+	People.call(this);
 }
 
-SuperType.prototype.getSuperValue = function () {
-	return this.property;
-};
-
-function SubType() {
-	this.subproperty = false;
+/**
+ * 组合继承 使用构造继承继承父类参数 使用原型继承继承父类函数
+ *
+ * 父类原型和子类原型是同一个对象 无法区分子类真正由谁构造
+ */
+function Man(name) {
+	People.call(this);
 }
 
-//继承了SuperType
-SubType.prototype = new SuperType();
-SubType.prototype.getSubValue = function () {
-	return this.subproperty;
-};
-let instance = new SubType();
-console.log(instance.getSuperValue());
+Man.prototype = People.prototype;
+
+/**
+ * 寄生组合继承 在组合继承的基础上 子类继承一个由父类原型生成的空对象
+ */
+function Man(name) {
+	People.call(this);
+}
+
+Man.prototype = Object.create(People.prototype, {
+	constructor: {
+		value: Man
+	}
+});
+
+/**
+ *
+ * @param ctor
+ * @param superCtor
+ */
+function inherits(ctor, superCtor) {
+	ctor.super_ = superCtor;
+	ctor.prototype = Object.create(superCtor.prototype, {
+		constructor: {
+			value: ctor,
+			enumerable: false,
+			writable: true,
+			configurable: true
+		}
+	});
+}

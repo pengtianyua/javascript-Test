5b8098927be8aee5410d0bff691010d5a9289eb6
Pengtianyu <2501846063@qq.com>
2022-01-28 16:18:37 +0800

    feat: 冒泡排序

--- src/JS/数组排序/bubbleSort.js
+++ src/JS/数组排序/bubbleSort.js
@@ -1,22 +1,32 @@
 /**
- * @method 冒泡排序
- * @description 遍历元素，跟其下一个元素对比 把最大的逐个往后排列
- * @param arr
- * @return arr
+ * 循环数组 比较当前元素和下一个元素 如果当前元素比下一个元素大 向上冒泡
+ * 这样一次循环之后最后一个数就是本数组最大的数
+ * 下一次循环继续上面的操作 不循环已经排好的数
+ * 当一次循环没有发生冒泡 说明排序已经完成 停止循环
+ *
+ * 时间复杂度 O(n2)
+ * 空间复杂度 O(1)
+ *
+ * @param array
+ * @returns {*}
  */
-function bubbleSort(arr) {
-	for (let i = 0; i < arr.length - 1; i++) {
-		for (let j = 0; j < arr.length - i - 1; j++) {
-			if (arr[j] > arr[j + 1]) {
-				let current = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = current;
+function bubbleSort(array) {
+	for (let j = 0; j < array.length; j++) {
+		let complete = true;
+		for (let i = 0; i < array.length - 1 - j; i++) {
+			// 比较相邻数
+			if (array[i] > array[i + 1]) {
+				[array[i], array[i + 1]] = [array[i + 1], array[i]];
+				complete = false;
 			}
 		}
+		// 没有冒泡结束循环
+		if (complete) {
+			break;
+		}
 	}
-	console.log(arr);
+	return array;
 }
 
-const arr = [12,3,44,343,55,1,23]
+const arr = [10, 8, 20, 5, 6, 30, 11, 9];
 bubbleSort(arr)
-

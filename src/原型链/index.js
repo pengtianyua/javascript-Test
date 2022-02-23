67048b9b00922c2ca3d91608ab37c1cf70492a9b
Pengtianyu <2501846063@qq.com>
2022-02-23 17:48:29 +0800

    feat: 原型链

--- src/原型链/index.js
+++ src/原型链/index.js
@@ -1,19 +1,18 @@
-function Person() {}
+function Person(name) {
+	this.name = name
+}
 
-const person = new Person();
+const p1 = new Person('ff')
 
-//构造函数的prototype属性指向原型对象
-console.dir(Person);
-//实例对象通过__proto__指针指向原型对象
-console.log("获取原型对象", Object.getPrototypeOf(person));
-console.log("实例对象通过__proto__指针指向原型对象	", person.__proto__ === Person.prototype);
-//构造函数通过__proto__指针指向构造函数的原型对象
-console.log("构造函数通过__proto__指针指向构造函数的原型对象", Person.__proto__ === Person.constructor.prototype);
-//原型对象的constructor属性指向构造函数
-console.log("原型对象的constructor属性指向构造函数", Person.prototype.constructor === Person);
-//原型对象通过__proto__指向对象的原型对象
-console.log("原型对象通过__proto__指向对象的原型对象", Person.prototype.__proto__ === Object.prototype);
-//对象的原型对象的constructor属性指向对象的构造函数
-console.log("对象的原型对象的constructor属性指向对象的构造函数", Object.prototype.constructor === Object);
-//对象的原型对象的原型对象为null
-console.log("对象的原型对象的原型对象为null", Object.prototype.__proto__ === null);d
+// p1原型链  p1 > Person.prototype > Object.prototype > null
+console.log(p1.__proto__ === Person.prototype);
+console.log(Person.prototype.__proto__ === Object.prototype);
+console.log(Object.prototype.__proto__ === null);
+
+// Person原型链  Person > Function.prototype > Object.prototype > null
+console.log(Person.__proto__ === Function.prototype);
+console.log(Function.prototype.__proto__ === Object.prototype);
+console.log(Object.prototype.__proto__ === null);
+
+
+// 实例对象隐式原型指向构造函数的显示原型 p1.__proto__ === Person.prototype

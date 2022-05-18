a24332a7f05611020db5f90d9814f190cc6bfe3c
v_pengtianyu <v_pengtianyu@baidu.com>
2022-05-18 16:44:31 +0800

    feat: 增加jsonp实现方式

--- src/手写/jsonp.js
+++ src/手写/jsonp.js
@@ -57,3 +57,14 @@ const jsonp = ({ url, params, callbackName }) => {
 		};
 	});
 };
+
+function jsonp(url, jsonpCallback, success) {
+	let script = document.createElement('script')
+	script.src = url
+	script.async = true
+	script.type = 'text/javascript'
+	window[jsonpCallback] = function(data) {
+		success && success(data)
+	}
+	document.appendChild(script)
+}

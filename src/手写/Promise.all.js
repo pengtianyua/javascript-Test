b6653a863f461b5e6ee05d7e2ceddadfc6dd6f1b
v_pengtianyu <v_pengtianyu@baidu.com>
2023-07-20 14:24:50 +0800

    feat: Promise.any

--- src/手写/Promise.all.js
+++ src/手写/Promise.all.js
@@ -32,3 +32,23 @@ function race(promiseArr) {
 		}
 	});
 }
+
+function pAny(promiseArr) {
+	return new Promise((resolve, reject) => {
+		const _promiseArr = Array.from(promiseArr);
+		const length = _promiseArr.length;
+		const rejectedArray = [];
+		_promiseArr.forEach((item) => {
+			Promise.resolve(item)
+				.then((r) => {
+					resolve(r);
+				})
+				.catch((reason) => {
+					rejectedArray.push(reason);
+					if (rejectedArray.length === length) {
+						reject(new AggregateError(rejectedArray));
+					}
+				});
+		});
+	});
+}

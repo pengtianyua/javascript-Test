52c014b0d071cae6a6bc6a837020b768bf56dbe5
v_pengtianyu <v_pengtianyu@baidu.com>
2023-08-13 17:04:13 +0800

    feat: 数组和树互转

--- src/Leetcode/array/array2Tree.js
+++ src/Leetcode/array/array2Tree.js
@@ -1,37 +1,37 @@
 function arr2Tree(arr) {
-	let res
-	const map = new Map()
-	arr.forEach(item => {
-		map.set(item.id, item)
-	)
-	arr.forEach(item => {
-		const parent = map.get(item.parentId)
-		if(parent) {
-			(parent.children || (parent.children = [])).push(item)
+	let res;
+	const map = new Map();
+	arr.forEach((item) => {
+		map.set(item.id, item);
+	});
+	arr.forEach((item) => {
+		const parent = map.get(item.parentId);
+		if (parent) {
+			(parent.children || (parent.children = [])).push(item);
 		} else {
-			res = item
+			res = item;
 		}
-	})
-	return res
+	});
+	return res;
 }
 
 function tree2Arr(tree) {
-	if(!tree.children) {
-		return tree
+	if (!tree.children) {
+		return tree;
 	}
-	const res = []
+	const res = [];
 	const traversalChildren = (children) => {
-		children.forEach(item => {
-			if(item.children) {
-				traversalChildren(item.children)
-				delete item.children
+		children.forEach((item) => {
+			if (item.children) {
+				traversalChildren(item.children);
+				delete item.children;
 			}
-			res.push(item)
-		})
-	}
+			res.push(item);
+		});
+	};
 
-	traversalChildren(tree.children)
-	delete tree.children
-	res.push(tree)
-	return res
+	traversalChildren(tree.children);
+	delete tree.children;
+	res.push(tree);
+	return res;
 }
